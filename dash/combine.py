8e6a68bd0cffd42c70b37f8f99242ccb07740598
Yashgupta03 <yash0310gupta@gmail.com>
2024-03-25 11:57:06 +0530

    v3.1

--- dash/combine.py
+++ dash/combine.py
@@ -19,8 +19,8 @@ app.layout = html.Div(children=[
 
         html.Div(className="six columns", children=[
             dcc.Dropdown(
-                options=["tree","choro","choro_specific"],
-                value="choro",
+                options=["Language Tree","Language Distribution","Language Specific"],
+                value="Language Distribution",
                 id="dataframe_dropdown",
                 style={"width": "40%"}
             )
@@ -29,13 +29,14 @@ app.layout = html.Div(children=[
 
     html.Br(),
     html.Div(id="img")
-    
 ])
 
 def generate_choro():
     color_map={
-        "NO": 'Blue',
-        "MARWARI" : 'Green',
+        "NO": 'Grey',
+        "DRAVIDIAN":"Grey",
+        "KHETRANI": 'Grey',
+        "SINO TIBETAN":"Grey",
     }
     fig = px.choropleth_mapbox(
                     df_data, 
@@ -63,6 +64,7 @@ def generate_choro():
     h_g2=dcc.Graph(id="state_lang")
     h_b1=dcc.Dropdown(options=np.append(df_lang['Languages'].dropna().unique(),["NO CHOOSEN",]),
                 value="NO CHOOSEN",
+                placeholder="Select a Language",
                 id="lang_dropdown",
                 style={"width": "40%"}
             )
@@ -133,7 +135,7 @@ def generate_choro_specific():
         Output("choro_lang", "figure"),
         [Input("major_lang_dropdown","value")]
 )
-def create_graph2(major_lang_dropdown):
+def create_choro_specific(major_lang_dropdown):
     df_sp1=df_data.copy()
     df_sp1.loc[df_sp1["Languages"] != major_lang_dropdown, "Languages"] = "Not Selected"
     color_map={
@@ -165,21 +167,16 @@ def create_graph2(major_lang_dropdown):
 @app.callback(
     Output("dist_lang", "figure"),
     [Input("lang_map", "clickData"),
-    Input("dataframe_dropdown", "value"),
-    Input("lang_dropdown","value")]
+    Input("dataframe_dropdown", "value")]
     # Input("lang_map", "hoverData"),
     # prevent_initial_call=True,
     # suppress_callback_exceptions=True
 )
-def create_graph(clickData,dataframe_dropdown,lang_dropdown):
-    if dataframe_dropdown=="tree":
+def create_graph(clickData,dataframe_dropdown):
+    if dataframe_dropdown=="Language Tree":
         return 
-    elif lang_dropdown!="NO CHOOSEN" :
-        df1=df_lang[df_lang["Languages"] == lang_dropdown]
-        fig = px.line(df1, x="Districts", y="Languages", markers=True)
-        return fig
     if clickData is None:
-        district_name = "GHAZIABAD"
+        district_name = "DELHI_TOTAL"
     else:
         district_name = clickData["points"][0]["location"]
 
@@ -187,6 +184,53 @@ def create_graph(clickData,dataframe_dropdown,lang_dropdown):
     fig = px.line(dff, x="Districts", y="Languages", markers=True)
     # fig.update_layout(width=600)
     return fig
+    df1=df_lang[df_lang["Districts"] == district_name]
+    opts = {
+        "tooltip": {
+            "trigger": "item",
+            "triggerOn": "mousemove",
+        },
+        "series": [
+            {
+                "type": "tree",
+                "data": [tree_data.data],
+                "top": "1%",
+                "left": "10%",
+                "bottom": "1%",
+                "right": "20%",
+                "symbolSize": 7,
+                "label": {
+                    "position": "bottom",
+                    "verticalAlign": "middle",
+                    "align": "right",
+                    "fontSize": 10,
+                    "color": "black",
+                },
+                "leaves": {
+                    "label": {
+                        "position": "right",
+                        "verticalAlign": "middle",
+                        "align": "left",
+                    }
+                },
+                # "emphasis": {
+                #     "focus": 'descendant'
+                # },
+                "expandAndCollapse": True,
+                "animationDuration": 550,
+                "animationDurationUpdate": 750,
+            },
+            
+        ],
+    }
+    dash_echarts.DashECharts(
+        option = opts,
+        id='echarts',
+        style={
+            "width": '100vw',
+            "height": '100vh',
+        }
+    )
 
 @app.callback(
         Output("lang_dropdown","value"),
@@ -198,13 +242,18 @@ def update_prev_click(clickData):
 @app.callback(
     Output("state_lang", "figure"),
     [Input("lang_map", "clickData"),
-    Input("dataframe_dropdown", "value")]
+    Input("dataframe_dropdown", "value"),
+    Input("lang_dropdown","value")]
 )
-def create_graph1(clickData,dataframe_dropdown):
-    if dataframe_dropdown=="tree":
+def create_graph1(clickData,dataframe_dropdown,lang_dropdown):
+    if dataframe_dropdown=="Language Tree":
         return 
+    elif lang_dropdown!="NO CHOOSEN" :
+        df1=df_lang[df_lang["Languages"] == lang_dropdown]
+        fig = px.line(df1, x="Districts", y="Languages", markers=True)
+        return fig
     if clickData is None:
-        state_name = "RAJASTHAN"
+        state_name = "DELHI"
     else:
         state_name = clickData["points"][0]["customdata"][0]
 
@@ -220,9 +269,9 @@ def create_graph1(clickData,dataframe_dropdown):
     # suppress_callback_exceptions=True
 )
 def update(dataframe_dropdown):
-    if dataframe_dropdown=="tree":
+    if dataframe_dropdown=="Language Tree":
         return generate_tree()
-    elif dataframe_dropdown=="choro":
+    elif dataframe_dropdown=="Language Distribution":
         return generate_choro()
     else:
         return generate_choro_specific()
